f274d45ff0b86838c92a37673b010553c25ab497
Shaaz Mukadam <84740787+Shaaz-Mukadam@users.noreply.github.com>
2023-06-02 00:23:20 +0530

    Add files via upload

---
+++ 3Invertedpyramid.cpp
@@ -0,0 +1,17 @@
+//Write a program for inverted pyramid using *
+#include<iostream>
+using namespace std;
+int main() {
+  int n;
+  cin>>n;
+
+    for(int i=n ; i>=1 ; i--){
+        for(int j=1 ; j<=i ; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+
+//**************OUTPUT*************
